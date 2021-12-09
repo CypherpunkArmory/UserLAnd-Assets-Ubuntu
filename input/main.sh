8bf2e20268b730fd74365d5a87a513ddb2ae6832
Corbin Champion <corbinlc@gmail.com>
2021-12-08 21:44:55 -0800

    add wget and curl

--- input/main.sh
+++ input/main.sh
@@ -15,7 +15,7 @@ apt-get update
 
 #install some packages with need for UserLAnd
 export DEBIAN_FRONTEND=noninteractive
-apt-get install -y --no-install-recommends sudo dropbear libgl1-mesa-glx tightvncserver xterm xfonts-base twm expect
+apt-get install -y --no-install-recommends sudo dropbear libgl1-mesa-glx tightvncserver xterm xfonts-base twm expect wget curl
 
 #clean up after ourselves
 apt-get clean
