9d2662962c3c491c84c42a0fad5110bc9c2241dd
Corbin Champion <corbinlc@gmail.com>
2022-01-21 17:21:32 -0800

    make VNC port a variable

--- assets/all/startVNCServerStep2.sh
+++ assets/all/startVNCServerStep2.sh
@@ -35,14 +35,18 @@ if [ ! -f /home/$INITIAL_USERNAME/.vncrc ]; then
 	echo $vncrc_line > /home/$INITIAL_USERNAME/.vncrc
 fi
 
-rm /tmp/.X51-lock
-rm /tmp/.X11-unix/X51
-tightvncserver -kill :51
-tightvncserver :51
+if [[ -z "${VNC_DISPLAY}" ]]; then
+  VNC_DISPLAY="51"
+fi
 
-while [ ! -f /home/$INITIAL_USERNAME/.vnc/localhost:51.pid ]
+rm /tmp/.X${VNC_DISPLAY}-lock
+rm /tmp/.X11-unix/X${VNC_DISPLAY}
+tightvncserver -kill :${VNC_DISPLAY}
+tightvncserver :${VNC_DISPLAY}
+
+while [ ! -f /home/$INITIAL_USERNAME/.vnc/localhost:${VNC_DISPLAY}.pid ]
 do
   sleep 1
 done
 cd ~
-DISPLAY=localhost:51 xterm -geometry 80x24+0+0 -e /bin/bash --login &
+DISPLAY=localhost:${VNC_DISPLAY} xterm -geometry 80x24+0+0 -e /bin/bash --login &
