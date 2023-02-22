67566657213eaf0db5839f46973828d4238756c1
Corbin Champion <corbinlc@gmail.com>
2023-02-21 21:45:01 -0800

    add scaling and sound support

--- input/main.sh
+++ input/main.sh
@@ -16,6 +16,7 @@ apt-get update
 #install some packages with need for UserLAnd
 export DEBIAN_FRONTEND=noninteractive
 apt-get install -y --no-install-recommends sudo dropbear libgl1-mesa-glx tightvncserver xterm xfonts-base twm expect wget curl
+apt-get install -y pulseaudio
 
 #clean up after ourselves
 apt-get clean
